090ce3e0c28f569fa356e8d156716bcc5693a101
Irshad-2 <irshadsajeena301@gmail.com>
2023-10-07 13:32:40 +0530

    updated some admin works

--- api/v1/questions/views.py
+++ api/v1/questions/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 
 
